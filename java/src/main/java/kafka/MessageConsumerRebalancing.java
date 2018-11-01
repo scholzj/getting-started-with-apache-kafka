81943fd35626d2d5e4c085ad8ff62448cae57168
Jakub Scholz <www@scholzj.com>
2018-11-01 17:56:53 +0100

    Use diferent group for rebalance

--- java/src/main/java/kafka/MessageConsumerRebalancing.java
+++ java/src/main/java/kafka/MessageConsumerRebalancing.java
@@ -22,7 +22,7 @@ public class MessageConsumerRebalancing implements Runnable {
     public MessageConsumerRebalancing(CountDownLatch latch)    {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "devnation-java");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "devnation-java-rebalance");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
